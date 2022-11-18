f73269412d601dd7e741e07b5557f7b49b68b3bc
Spiekermann, Maximilian <maximilian.spiekermann@zuehlke.com>
2022-11-18 14:30:22 +0100

    Added toolbar and navigation animations

--- app/src/main/java/de/unihannover/hci/menudetector/MainActivity.kt
+++ app/src/main/java/de/unihannover/hci/menudetector/MainActivity.kt
@@ -3,11 +3,24 @@ package de.unihannover.hci.menudetector
 // Android
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+
+// Google Material
+import com.google.android.material.appbar.MaterialToolbar
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        val toolbar: MaterialToolbar = findViewById(R.id.toolbar)
+        toolbar.setupWithNavController(navController, appBarConfiguration)
     }
 }
