b70db25ebfc305bd71d8852d6c0aa9351881e8a7
Taher <t.arab@stud.uni-hannover.de>
2022-12-06 13:48:53 +0100

    Dropdown languages

--- app/src/main/java/de/unihannover/hci/menudetector/fragments/SettingsFragment.kt
+++ app/src/main/java/de/unihannover/hci/menudetector/fragments/SettingsFragment.kt
@@ -1,6 +1,9 @@
 package de.unihannover.hci.menudetector.fragments
 
 // Android
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import androidx.fragment.app.Fragment
 
 // Internal dependencies
@@ -11,7 +14,7 @@ import de.unihannover.hci.menudetector.R
  * TODO:
  * - Add options to dropdown
  * - Select system language default
- * - Remove start language dropdown
+ * - Remove start language dropdown (done)
  * - Store current value in SharedPreferences
  * - Optional: Converter class between money and/or weight units
  */
