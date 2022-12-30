619ac0626ca4c1789ebfda459b370617bec89b8d
Taher <t.arab@stud.uni-hannover.de>
2022-12-30 12:54:41 +0100

    Info Order completed

---
+++ app/src/main/java/de/unihannover/hci/menudetector/fragments/info/OrderInfo.kt
@@ -0,0 +1,27 @@
+package de.unihannover.hci.menudetector.fragments.info
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.google.android.material.button.MaterialButton
+import de.unihannover.hci.menudetector.R
+
+class OrderInfo: DialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        var rootView: View = inflater.inflate(R.layout.fragment_info_order,container,false)
+
+        val closeButton: MaterialButton = rootView.findViewById(R.id.info_order_close_btn)
+        closeButton.setOnClickListener {
+            dismiss()
+        }
+        return rootView
+    }
+
+}
